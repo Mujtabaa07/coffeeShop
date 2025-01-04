eda04f5fae59a7b29bd3ee73ba7eddfd8ed6485f
Sohini_Das <sohinid474@gmail.com>
2025-01-05 00:27:57 +0530

    Fixed posotioning of social icons and copyright

--- src/componets/footer.js
+++ src/componets/footer.js
@@ -20,7 +20,7 @@ const FooterContent = styled.div`
   
   p {
     margin: 0.5rem 0;
-    font-size: 1rem;
+    font-size: 0.75rem;
     line-height: 1.6;
     
     &:first-child {
@@ -96,31 +96,8 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterContent>
-        <p>&copy; {new Date().getFullYear()} MsCafe. All rights reserved.</p>
-        <p>Made with ♥ by Mscoder</p>
-
-        {/* Informational Sections */}
-        <InfoSection>
-          <InfoColumn>
-            <h3>About Us</h3>
-            <p>Founded in 2010, MsCafe is dedicated to serving the finest coffee with passion and expertise. We source our beans from sustainable farms across the globe.</p>
-          </InfoColumn>
-
-          <InfoColumn>
-            <h3>Contact Us</h3>
-            <p>Email: contact@mscafe.com</p>
-            <p>Phone: (123) 456-7890</p>
-          </InfoColumn>
-
-          <InfoColumn>
-            <h3>Location</h3>
-            <p>123 Coffee St, Bean Town, USA</p>
-            <p><a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer">View on Map</a></p>
-          </InfoColumn>
-        </InfoSection>
-
-        {/* Social Icons */}
-        <SocialIcons>
+       {/* Social Icons */}
+       <SocialIcons>
           <SocialIcon
             href="https://facebook.com"
             target="_blank"
@@ -152,6 +129,28 @@ function Footer() {
             <i className="fab fa-instagram"></i>
           </SocialIcon>
         </SocialIcons>
+
+        {/* Informational Sections */}
+        <InfoSection>
+          <InfoColumn>
+            <h3>About Us</h3>
+            <p>Founded in 2010, MsCafe is dedicated to serving the finest coffee with passion and expertise. We source our beans from sustainable farms across the globe.</p>
+          </InfoColumn>
+
+          <InfoColumn>
+            <h3>Contact Us</h3>
+            <p>Email: contact@mscafe.com</p>
+            <p>Phone: (123) 456-7890</p>
+          </InfoColumn>
+
+          <InfoColumn>
+            <h3>Location</h3>
+            <p>123 Coffee St, Bean Town, USA</p>
+            <p><a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer">View on Map</a></p>
+          </InfoColumn>
+        </InfoSection>
+        <p>&copy; {new Date().getFullYear()} MsCafe. All rights reserved.</p>
+        <p>Made with ♥ by Mscoder</p>
       </FooterContent>
     </FooterContainer>
   );

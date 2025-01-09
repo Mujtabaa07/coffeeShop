958b27c16928329f74a464c93dcd74f43d0c579c
Sohini_Das <sohinid474@gmail.com>
2025-01-09 19:34:34 +0530

    Made changes to remove error

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true, 
-    environment: 'jsdom',  
-  
+  build: {
+    outDir: 'build', // Change output directory to 'build'
   },
 });

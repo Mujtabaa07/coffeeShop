03b6da10f6b0f12686c8608ae4b8656ce48bbfd2
Aditi Tangri <adititangri5@gmail.com>
2025-09-06 19:15:44 +0530

    Fixed: Enhancement: Modernize and Improve Footer Section Design

--- src/componets/footer.js
+++ src/componets/footer.js
@@ -412,7 +412,7 @@ function Footer() {
               <Link to="/about" style={{textAlign:'center'}}>About</Link>
               <Link to="/blog" style={{textAlign:'center'}}>Blog</Link>
               <Link to="/contact" style={{textAlign:'center'}}>Contact</Link>
-              <Link to="/testimonial">Testimonial</Link>
+              <Link to="/testimonial" style={{textAlign:'center'}}>Testimonial</Link>
               <Link to="/contributor">Contributors</Link>
             </InfoColumn>
 
